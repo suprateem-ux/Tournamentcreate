8ea0d39225543541f5ad1d12171ce1e1b415c4ce
suprateem-ux <supraontwitch708@gmail.com>
2025-07-14 18:06:05 +0530

    Update Swiss.py

--- Swiss.py
+++ Swiss.py
@@ -6,7 +6,7 @@ import pathlib
 import json
 
 # === Config ===
-API_TOKEN = os.getenv("KEY")
+API_TOKEN = os.environ("KEY")
 TEAM_ID = "international-chess-talent"
 DESC_FILE = pathlib.Path(__file__).with_name("description.txt")
 
